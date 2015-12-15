858551d7fd1dcb64cffde78c13c3e941c416d882
black.bao <1248264627@qq.com>
2015-12-15 12:58:33 +0800

    第一次提交

--- app/src/main/java/cn/jfpal/www/testapplication/MainActivity.java
+++ app/src/main/java/cn/jfpal/www/testapplication/MainActivity.java
@@ -4,13 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        btn = findViewById(R.id.)
+
     }
 
     @Override
